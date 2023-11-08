ae1f9fb4c477a9ba27f66590e8942e814e581ae6
Dipesh B C <bcdipeshwork@gmail.com>
2023-11-07 19:18:06 -0500

    Fixed a bug in template

--- app.py
+++ app.py
@@ -390,22 +390,3 @@ def homepage():
 
     else:
         return render_template("home-anon.html")
-
-
-##############################################################################
-# Turn off all caching in Flask
-#   (useful for dev; in production, this kind of stuff is typically
-#   handled elsewhere)
-#
-# https://stackoverflow.com/questions/34066804/disabling-caching-in-flask
-
-
-@app.after_request
-def add_header(req):
-    """Add non-caching headers on every request."""
-
-    req.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    req.headers["Pragma"] = "no-cache"
-    req.headers["Expires"] = "0"
-    req.headers["Cache-Control"] = "public, max-age=0"
-    return req
